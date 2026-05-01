b6d087a9db27467e97e85ae6802562cc3a4670af
salfonsoMB17 <serggios17@gmail.com>
2026-05-01 13:07:49 -0500

    Exercise 3.4: Implement DELETE /api/persons/:id endpoint

--- index.js
+++ index.js
@@ -48,6 +48,11 @@ app.get('/api/persons/:id', (req, res) => {
   }
 })
 
+app.delete('/api/persons/:id', (req, res) => {
+  persons = persons.filter(p => p.id !== Number(req.params.id))
+  res.status(204).end()
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
