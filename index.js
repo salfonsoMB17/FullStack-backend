400d1271989c7a97678ae9a18636303758ded4b2
salfonsoMB17 <serggios17@gmail.com>
2026-05-09 15:15:14 -0500

    Exercise 3.11: Serve frontend production build from backend

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('data', function getData (req) {
   if (req.method === 'POST'){
@@ -35,10 +36,6 @@ let persons = [
     "number": "39-23-6423122"
   }
 ]
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
 app.get('/api/persons', (request, response) => {
   response.json(persons)
