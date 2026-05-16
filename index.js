d450b9ec224e532b4e974253bda762ff412719ad
salfonsoMB17 <serggios17@gmail.com>
2026-05-16 14:43:31 -0500

    Exercise 3.19: Add validation for name minimum length with error handling

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ app.put('/api/persons/:id', (req, res, next) => {
     number: number,
   }
 
-  Person.findByIdAndUpdate(req.params.id, person, { new: true })
+  Person.findByIdAndUpdate(req.params.id, person, { new: true, runValidators: true, context: 'query' })
     .then(updatedPerson => {
       res.json(updatedPerson)
     })
@@ -91,7 +91,9 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
